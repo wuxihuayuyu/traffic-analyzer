0980117bd056f75c21793e3f2846e0d4052091f4
wuxihuayuyu feng <2656530295@qq.com>
2026-09-08 16:30:11 +0800

    parse IP layer fields

--- sniffer.py
+++ sniffer.py
@@ -1,8 +1,16 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP
+
+# IP头里的协议号是数字不是名字：6=TCP，17=UDP，1=ICMP
+PROTOCOLS = {1: "ICMP", 6: "TCP", 17: "UDP"}
 
 
 def handle_packet(pkt):
-    print(pkt.summary())
+    if IP in pkt:
+        ip = pkt[IP]
+        proto = PROTOCOLS.get(ip.proto, "其他")
+        print(f"{ip.src} > {ip.dst}  协议: {proto}")
+    else:
+        print("(这个包没有IP层，比如ARP)")
 
 
 if __name__ == "__main__":
